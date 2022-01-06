f06faab5cb9dab0e67f58b256f4a437d63a12573
Ankush Minda <ankushminda@users.noreply.github.com>
2022-01-06 11:50:54 +0530

    Create random-bg.js

---
+++ random-bg.js
@@ -0,0 +1,7 @@
+
+
+function randomBGColor() {
+  var items = Array("#00b894", "#0984e3","#ffeaa7","#ff7675", "#e17055", "#€84393", "#2d3436");
+  var item = items (Math. floor(Math.random()*items. Length)]:
+  document.body.style.background = item;
+}
